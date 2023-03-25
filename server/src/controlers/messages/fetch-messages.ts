1aa86feee29c6b121f88d05b141a8b914e9ef3a2
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-03-25 16:21:22 -0600

    added comments to fetch-messages

--- server/src/controlers/messages/fetch-messages.ts
+++ server/src/controlers/messages/fetch-messages.ts
@@ -1,17 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import { Message, Room } from "../../model";
+import { BadRequest } from "../../errors";
 
 const getMessagesByRoomId = async (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
+	// Get roomId from params
 	const { roomId } = req.params;
 
+	// Check if roomId is valid
+	if (!roomId) {
+		return next(new BadRequest("RoomId is required"));
+	}
+
+	// Find room by roomId and populate messages
 	const messages = await Room.findOne({ _id: roomId }).populate({
 		path: "messages",
 	});
 
+	// Send messages to client
 	res.status(200).json(messages);
 };
 
