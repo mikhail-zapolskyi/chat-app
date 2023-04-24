c5b59ef7a8e7882e63babf51886263737fa9d20c
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-04-24 17:00:40 -0600

    deleted dist folder

--- server/dist/src/controlers/auth/logout.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logout = (req, res, next) => {
-    req.session = null;
-    res.status(200).json({ user: null, message: "You have been logged out" });
-};
-exports.default = logout;
-//# sourceMappingURL=logout.js.map
--- server/dist/src/controlers/auth/user.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user = (req, res, next) => {
-    res.status(200).json({ user: req["user"] || null });
-};
-exports.default = user;
-//# sourceMappingURL=user.js.map
